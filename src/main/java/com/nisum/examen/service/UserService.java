c868ba5b143ca837642755df643219942cc9c0cb
Lucas <lucas.salvador@ticnow.cl>
2025-03-22 14:28:03 -0300

    impor transactional

--- src/main/java/com/nisum/examen/service/UserService.java
+++ src/main/java/com/nisum/examen/service/UserService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
 import org.springframework.data.domain.Page;
